bbc8f14ff07504cf93780fb7b2d6bd1df144d35c
jarek <jarek.mokrzycki@gmail.com>
2022-01-07 00:53:11 +0100

    basic map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
- 
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const position = [51.505, -0.09];
+
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+
+      <MapContainer 
+      style={{ width: '100vw', height: '100vh' }}
+      center={position}
+      zoom={3}
+      scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+    </MapContainer>
+
   );
 }
  
