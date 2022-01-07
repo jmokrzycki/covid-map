b5e4c6df55b36a94d5ee42dc6c722a919ca79c9d
jarek <jarek.mokrzycki@gmail.com>
2022-01-07 02:09:22 +0100

    countries added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import axios from 'axios';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+
+
 const position = [51.505, -0.09];
 
 function App() {
-  return (
+  const [countries, setCountries] = useState([]);
 
+  useEffect(()=>{
+    console.log(countries)
+  })
+
+  useEffect(() => {
+    axios.get('https://disease.sh/v3/covid-19/countries')
+    .then((response) => {
+      setCountries(Object.values(response.data));  
+    })
+  }, []);
+
+  return (
       <MapContainer 
       style={{ width: '100vw', height: '100vh' }}
       center={position}
@@ -18,13 +34,18 @@ function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {
+         countries.length && countries.map(({countryInfo}, index) => {
+            return (
+              <Marker key={index} position={[countryInfo.lat, countryInfo.long]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            )
+          })
+        }
     </MapContainer>
-
   );
 }
  
